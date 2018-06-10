1bc542852190d4a0cb48f9bc07eda5ee3f12382f
entropia.studio <info@entropia.studio>
2018-06-10 16:04:06 +0000

    Modify use to mLab collection

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 const ENV = {
-    "USER"      : "entropia",
-    "PASS"      : "Sy8tOTbT",
+    "USER"      : "freecodecamp",
+    "PASS"      : "Wx1PQwJE",
     "HOST"      : "ds253960.mlab.com",
     "DB_PORT"   : "53960",
     "DB"        : "freecodecamp",
